61131e369f7c6b9a17736ed62e53ebcff824cf60
Renee Hickman <renee.hickman@outlook.com>
2019-02-20 16:01:12 -0600

    concert-this

--- liri.js
+++ liri.js
@@ -4,11 +4,10 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 //Using the Spotify api and getting the key from keys.js
-var Spotify = require('node-spotify-api'); 
+var Spotify = require('node-spotify-api');
 
 //load the spotify keys
 var spotify = new Spotify(keys.spotify);
-
 
 //Required to use moment for node
 var moment = require('moment');
@@ -19,11 +18,14 @@ var axios = require("axios");
 // Load the fs package to read and write
 var fs = require("fs");
 
+var logline = "----------------------------------"
+
+
 // Take two arguments.
 // The first will be the action (i.e. "concert", "spotify", etc.)
 // The second will be the value/name of band/song/movie
 var action = process.argv[2];
-var value = process.argv.slice(3).join("+");
+var value = process.argv.slice(3).join(" ");
 
 
 // switch-case will direct which function gets run.
@@ -48,37 +50,50 @@ switch (action) {
 }
 
 
+
+function logData(data) {
+    fs.appendFile("log.txt", data, function (error) {
+        if (error) {
+            console.log(error);
+        }
+    });
+}
+
+
+
 //Funtion for concert-this Bands in Town API
 function concert(value) {
+    var actionText = ('\n' + logline + '\n' + action + " : " + value + '\n' + logline);
+
+    console.log(actionText);
+    logData(actionText);
 
     var queryUrl = "https://rest.bandsintown.com/artists/" + value + "/events?app_id=" + keys.bandsintown.id + '"';
 
     axios.get(queryUrl).then(
         function (response) {
+            if (value == null) {
+                var nullText = ("\nSorry. It looks like that artist isn't on the road. Please search again...");
+                console.log(nullText);
+                logData(nullText);
+            } else {
 
-            for (var i = 0; i < response.data.length; i++) {
-                console.log("\nVenue Name: " + response.data[i].venue.name +
-                    "\nVenue Location: " + response.data[i].venue.city +
-                    "\nDate of the Event: " + moment(response.data[i].datetime).format("MM/DD/YYYY")); 
+                var introText = ('\nUpcoming concerts for ' + value + "\n" + logline);
+                console.log(introText);
+                logData(introText);
+
+                for (var i = 0; i < response.data.length; i++) {
+                    var outputData = ('\nVenue Name: ' + response.data[i].venue.name + '\nVenue Location: ' + response.data[i].venue.city + ', ' + response.data[i].venue.country + '\nDate of the Event: ' + moment(response.data[i].datetime).format("MM/DD/YYYY") + "\n");
+                    console.log(outputData);
+                    logData(outputData);
+                }
             }
-     
         })
         .catch(function (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
-            }
-            console.log(error.config);
+            console.log(error);
+            var errorText = ("\nSorry that artist couldn't be found. Please search again...");
+            console.log(errorText);
+            logData(errorText);
         });
 
 }
